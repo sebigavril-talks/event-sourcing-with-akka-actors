2e41ded07fe9a65a7a17fe64a37e20b38c2c5c84
Sebastian Gavril <sebigavril@users.noreply.github.com>
2018-03-15 17:40:49 +0200

    Add demo for actos basics (#1)

--- src/main/scala/com/sebigavriltalks/ThermometerActor.scala
+++ src/main/scala/com/sebigavriltalks/ThermometerActor.scala
@@ -5,9 +5,19 @@ import scala.concurrent.ExecutionContext.Implicits.global
 import scala.util.Random
 
 import akka.actor.Actor
+import com.sebigavriltalks.ThermometerActorProtocol.Tick
+import com.sebigavriltalks.ThermometerReader.{Read, Reading}
 
 class ThermometerActor extends Actor {
-  override def receive: Receive = Actor.emptyBehavior
+
+  private var temperature = 0
+
+  context.system.scheduler.schedule(1.seconds, 1.seconds, self, Tick)
+
+  override def receive: Receive = {
+    case Tick  => temperature = temperature + nextTempIncrement()
+    case Read  => sender() ! Reading(temperature)
+  }
 
   // (+1 / 0 / -1)
   private def nextTempIncrement() = Random.nextInt(3) - 1
