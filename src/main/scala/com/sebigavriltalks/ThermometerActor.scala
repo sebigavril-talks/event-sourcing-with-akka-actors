fa4bb8d4fc0cf54d701dc66a29327afd467ea67b
Sebastian Gavril <sgavril@wehkamp.nl>
2018-03-22 22:03:16 +0200

    Add demo for akka persistence

--- src/main/scala/com/sebigavriltalks/ThermometerActor.scala
+++ src/main/scala/com/sebigavriltalks/ThermometerActor.scala
@@ -4,16 +4,30 @@ import scala.concurrent.duration._
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.util.Random
 
-import akka.actor.Actor
+import akka.persistence.PersistentActor
 
-class ThermometerActor extends Actor {
+class ThermometerActor(id: String) extends PersistentActor {
   private var temperature = 0
 
   context.system.scheduler.schedule(1.seconds, 1.seconds, self, "tick")
 
-  override def receive: Receive = {
-    case "tick"  => temperature = temperature + nextTempIncrement()
+  override def persistenceId: String = id
+
+  override def receiveCommand: Receive = {
     case "read"  => sender() ! temperature
+    case "tick"  => persist(nextTempIncrement()) {
+      increment => updateState(increment)
+    }
+  }
+
+  override def receiveRecover: Receive = {
+    case increment: Int =>
+      println(s"Recovering with $increment")
+      updateState(increment)
+  }
+
+  private def updateState(event: Any) = event match {
+    case increment: Int => temperature = temperature + increment
   }
 
   // (+1 / 0 / -1)
